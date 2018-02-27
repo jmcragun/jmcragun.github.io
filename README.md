05dff2527ebb56837b82de86239be49db1a643d4
jmcragun <jmcragun@gmail.com>
2018-02-27 13:53:24 -0700

    Initial Commit
    help

---
+++ README.md
@@ -0,0 +1,2 @@
+# Resume
+Learn a little more about this cool son of a gun. (Please. I need money (; _ ;))
---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $("#photo").hide().fadeIn(1500);
+  $("h1").hide().fadeIn(1500);
+  $(".small").hide().fadeIn(1500);
+  $(".belowtop").hide().fadeIn(1500);
+  $("main").hide().delay(1500).fadeIn(1500);
+});
